5c5d98853492d43af9471bb191ee1ec0799ae06c
kylead10 <101107354+kylead10@users.noreply.github.com>
2022-06-17 20:02:41 +0100

    Update get_builtin.c

--- get_builtin.c
+++ get_builtin.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_builtin - builtin that pais the command in the arg
+ * get_builtin - builtin that pais the command in the arg.
  * @cmd: command
- * Return: function pointer of the builtin command
+ * Return: function pointer of the builtin command.
  */
 int (*get_builtin(char *cmd))(data_shell *)
 {
