026b9f85725b5ee65ba303db6be8331f9f5ab897
kylead10 <101107354+kylead10@users.noreply.github.com>
2022-06-17 20:01:04 +0100

    Update aux_str3.c

--- aux_str3.c
+++ aux_str3.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string.
- * @s: input string.
- * Return: no return.
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: no return
  */
 void rev_string(char *s)
 {
